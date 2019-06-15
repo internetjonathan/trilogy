75696e7a3d0c0b27986fbed804a30e13e56fa83f
Jonathan <miami.dionne@gmail.com>
2019-06-15 00:41:33 -0400

    hopefully final push

---
+++ app/js/btn.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('button').on("click", function () {
+        console.log('I would love to work with you!');
+        // $(this).toggleClass('open')
+    });
+});
